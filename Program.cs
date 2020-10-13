0765592cdbb284f3378830d876fece3e2b6b2226
Ben W <bwiedeback@gmail.com>
2020-10-13 13:22:49 -0600

    initialize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
